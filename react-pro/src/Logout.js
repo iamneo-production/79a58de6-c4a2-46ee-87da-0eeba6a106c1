ebf30b737a048c0fc9c8732408e01e53d26ac2cc
manideepshiligam <111494942+manideepshiligam@users.noreply.github.com>
2022-09-30 14:15:46 +0530

    Delete react-pro directory

--- react-pro/src/Logout.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import  {Navigate} from 'react-router-dom';
-
-const Logout=()=>{
-    localStorage.clear();
-    return <Navigate to='/Login' />
-};
-export default Logout;
